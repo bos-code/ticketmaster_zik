8c5ebd82a7645452a3f69991c6f85d12487468aa
Henry chidera <chidera9713@gmail.com>
2026-05-02 11:36:28 +0100

    chore: remove Clerk integration and related environment variables

--- src/lib/clerk.ts
+++
@@ -1,9 +0,0 @@
-const maybeClerkPublishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
-
-if (!maybeClerkPublishableKey) {
-  throw new Error(
-    'Missing EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY. Add it to your local .env file.',
-  );
-}
-
-export const clerkPublishableKey: string = maybeClerkPublishableKey;
